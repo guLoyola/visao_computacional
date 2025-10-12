e22e3a881a6bb4552e727efe3a708f6f5435d4b9
diogoalmd <diogo.meida@gmail.com>
2025-10-12 20:38:12 -0300

    feat: tomato potato and carrot mobilenetv2 run

--- data/download_dataset.py
+++ data/download_dataset.py
@@ -1,5 +1,13 @@
 import kagglehub
+import certifi
+import os
 
-path = kagglehub.dataset_download("shifatearman/bananalsd")
+# Solução para problema de certificados SSL
+# Configura o caminho dos certificados do certifi
+os.environ['SSL_CERT_FILE'] = certifi.where()
+os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
+
+# Download latest version
+path = kagglehub.dataset_download("muhammad0subhan/fruit-and-vegetable-disease-healthy-vs-rotten")
 
 print("Path to dataset files:", path)
