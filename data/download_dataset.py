80dcb3c209b2d6545239e18e801cdd0987aba1d4
diogoalmd <diogo.meida@gmail.com>
2025-10-19 19:26:37 -0300

    feat: dataset

--- data/download_dataset.py
+++ data/download_dataset.py
@@ -1,5 +1,7 @@
 import kagglehub
 
-path = kagglehub.dataset_download("shifatearman/bananalsd")
+# path = kagglehub.dataset_download("shifatearman/bananalsd")
+
+path = kagglehub.dataset_download("asadullahgalib/guava-disease-dataset")
 
 print("Path to dataset files:", path)
